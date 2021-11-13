389dcc0b5cfbf85781276fc0a6a24254b8e56770
Shaho Farivar <56560445+havram16@users.noreply.github.com>
2021-11-13 10:46:27 +0330

    Update PigLatin.py
    Add finding signs at the end of each word.
    Add comments.

--- PigLatin.py
+++ PigLatin.py
@@ -9,17 +9,25 @@ numbers="0123456789"
 pigLatinList=[]
 pigLatin=""
 wordCons=""
-
+wordSign=""
 
 sentence=input("Enter your sentence:\n")
 sentenceList=sentence.split(" ")
-#print(sentenceList) #del
 
 for word in sentenceList:
+  #check not alphabetics:
+  if not word[-1].isalpha():
+    wordSign=word[-1]
+    word=word[:-1]
+    print(wordSign,"--",word)
+  #check if start with vowel
   if word[0].lower() in vowels:
-    pigLatinList.append(word+vowelString)
+    pigLatinList.append(word+vowelString+wordSign)
+  #check if start with numbers
   elif word[0] in numbers:
+    word+=wordSign
     pigLatinList.append(word)
+  #check if start with consonant cluster
   else:
     counter=0
     for item in word:
@@ -27,13 +35,18 @@ for word in sentenceList:
         counter+=1
       else:
         break
-    for i in range(counter,len(word)):
+    #finding first vowel and words after it
+    for i in range(counter,len(word)): 
       wordCons+=word[i]
+    #finding consonant cluster
     for k in range(counter):
       wordCons+=word[k]
     wordCons+=consString
+    wordCons+=wordSign
     pigLatinList.append(wordCons)
     wordCons=""
+  wordSign=""
+  
        
 pigLatin=" ".join(pigLatinList)
 print(pigLatin)
